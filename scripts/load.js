05d954e8460ce2caf6b5d2ffa8ba6e47d6891a39
lzfgit-byte <1807409742@qq.com>
2023-07-17 14:56:43 +0800

    test

--- scripts/load.js
+++ scripts/load.js
@@ -2,13 +2,8 @@ const { ensureFileSync } = require('fs-extra');
 const { writeFileSync, readFileSync } = require('node:fs');
 const { axiosGet } = require('./http');
 ensureFileSync('./txt.txt');
-// const data = readFileSync('./3.html');
-// writeFileSync('./txt.txt', data);
-// axiosGet('https://www.baidu.com').then((res) => {
-//   console.log('res', res);
+
+// axiosGet('https://www.zhihu.com/billboard').then((res) => {
 //   writeFileSync('./txt.txt', res);
 // });
-setTimeout(() => {
-  writeFileSync('./txt.txt', '123456wwwwasdasd');
-}, 30000);
 module.exports = {};
